0eaedf872d5421f573bd3ddd5db98ed403ebd394
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2021-09-21 23:19:07 +0000

    use valid_move to check if cornelius is about to run into the walls.

--- src/logic.rs
+++ src/logic.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 
 use log::info;
 
-use crate::{Battlesnake, Board, Game};
+use crate::{Battlesnake, Board, Coord, Game};
 
 pub fn get_info() -> JsonValue {
     info!("INFO");
@@ -27,7 +27,7 @@ pub fn end(game: &Game, _turn: &u32, _board: &Board, _you: &Battlesnake) {
     info!("{} END", game.id);
 }
 
-pub fn get_move(game: &Game, _turn: &u32, _board: &Board, you: &Battlesnake) -> &'static str {
+pub fn get_move(game: &Game, _turn: &u32, board: &Board, you: &Battlesnake) -> &'static str {
     let mut possible_moves: HashMap<_, _> = vec![
         ("up", true),
         ("down", true),
@@ -56,8 +56,26 @@ pub fn get_move(game: &Game, _turn: &u32, _board: &Board, you: &Battlesnake) -> 
 
     // TODO: Step 1 - Don't hit walls.
     // Use board information to prevent your Battlesnake from moving beyond the boundaries of the board.
-    // board_width = move_req.board.width
-    // board_height = move_req.board.height
+    let left = |head: &Coord| Coord {
+        x: head.x - 1,
+        y: head.y,
+    };
+    let right = |head: &Coord| Coord {
+        x: head.x + 1,
+        y: head.y,
+    };
+    let up = |head: &Coord| Coord {
+        x: head.x,
+        y: head.y + 1,
+    };
+    let down = |head: &Coord| Coord {
+        x: head.x,
+        y: head.y - 1,
+    };
+    possible_moves.insert("left", valid_move(&left(&my_head), &board));
+    possible_moves.insert("right", valid_move(&right(&my_head), &board));
+    possible_moves.insert("up", valid_move(&up(&my_head), &board));
+    possible_moves.insert("down", valid_move(&down(&my_head), &board));
 
     // TODO: Step 2 - Don't hit yourself.
     // Use body information to prevent your Battlesnake from colliding with itself.
@@ -83,4 +101,92 @@ pub fn get_move(game: &Game, _turn: &u32, _board: &Board, you: &Battlesnake) -> 
     info!("{} MOVE {}", game.id, chosen);
 
     return chosen;
+}
+
+fn valid_move(spot: &Coord, board: &Board) -> bool {
+    let board_width = board.width;
+    let board_height = board.height;
+
+    match spot {
+        Coord { y: 0, .. } => { println!("down"); return false; },
+        Coord { x: 0, .. } => { println!("left"); return false; },
+        Coord { y, .. } if y == &board_width => { println!("right"); return false; }, // Rust is weird
+        Coord { x, .. } if x == &board_height => { println!("up"); return false; },
+        _ => { return true; }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn head_will_not_hit_left_wall() {
+        let board = Board {
+            width: 10,
+            height: 10,
+            food: vec![],
+            hazards: vec![],
+            snakes: vec![],
+        };
+        let spot = Coord { x: 0, y: 5 };
+        let valid_move = valid_move(&spot, &board);
+        assert_eq!(valid_move, false);
+    }
+
+    #[test]
+    fn head_will_not_hit_right_wall() {
+        let board = Board {
+            width: 10,
+            height: 10,
+            food: vec![],
+            hazards: vec![],
+            snakes: vec![],
+        };
+        let spot = Coord { x: 10, y: 5 };
+        let valid_move = valid_move(&spot, &board);
+        assert_eq!(valid_move, false);
+    }
+
+    #[test]
+    fn head_will_not_hit_roof() {
+        let board = Board {
+            width: 10,
+            height: 10,
+            food: vec![],
+            hazards: vec![],
+            snakes: vec![],
+        };
+        let spot = Coord { x: 5, y: 10 };
+        let valid_move = valid_move(&spot, &board);
+        assert_eq!(valid_move, false);
+    }
+
+    #[test]
+    fn head_will_not_hit_floor() {
+        let board = Board {
+            width: 10,
+            height: 10,
+            food: vec![],
+            hazards: vec![],
+            snakes: vec![],
+        };
+        let spot = Coord { x: 5, y: 0 };
+        let valid_move = valid_move(&spot, &board);
+        assert_eq!(valid_move, false);
+    }
+
+    #[test]
+    fn head_will_travel() {
+        let board = Board {
+            width: 10,
+            height: 10,
+            food: vec![],
+            hazards: vec![],
+            snakes: vec![],
+        };
+        let spot = Coord { x: 5, y: 5 };
+        let valid_move = valid_move(&spot, &board);
+        assert_eq!(valid_move, true);
+    }
 }
