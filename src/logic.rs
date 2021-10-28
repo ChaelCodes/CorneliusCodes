d809f695d1db6c99bea67a7c8c62f3460b568588
Rachael Wright-Munn <chaelcodes@gmail.com>
2021-10-28 20:57:44 +0000

    Corney sheds their skin

--- src/logic.rs
+++ src/logic.rs
@@ -12,7 +12,7 @@ pub fn get_info() -> JsonValue {
     return json!({
         "apiversion": "1",
         "author": "ChaelCodes",
-        "color": "#F6661E",
+        "color": "#c88b4c",
         "head": "bendr",
         "tail": "round-bum",
     });
